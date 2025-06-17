a170f475437f78b25db6b699faf85d5ac221dd44
JoseVidal1 <jose.vidal.ttc.182@unilibre.edu.co>
2025-06-17 10:54:23 -0500

    Agregar archivos de proyecto.

---
+++ Datos/BaseDatos.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class BaseDatos
+    {
+        string cadenaConexion = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+
+        protected MySqlConnection conexion;
+        public BaseDatos()
+        {
+            conexion = new MySqlConnection(cadenaConexion);
+
+        }
+        public bool AbrirConexion()
+        {
+            if (conexion.State != ConnectionState.Open)
+            {
+                conexion.Open();
+                return true;
+            }
+            return false;
+        }
+
+        public void CerrarConexion()
+        {
+            conexion.Close();
+        }
+
+        public MySqlConnection ObtenerConexion()
+        {
+            return conexion;
+        }
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public int id { get;set; }
+        public string name { get;set; }
+        public string usuario { get;set; }
+        public string contraseña { get;set;}
+        public string rol { get;set;}
+        public string estado { get;set;}
+    }
+}
